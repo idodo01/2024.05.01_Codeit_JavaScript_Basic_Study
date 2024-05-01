bb415196537460ff1ae64af9088ce17d2a5966fd
IDO <susususu07@naver.com>
2024-05-01 14:52:22 +0900

    Add files via upload

---
+++ elementaryArithmetic.js
@@ -0,0 +1,21 @@
+//덧셈
+console.log(4 + 1);
+
+// 뺄셈
+console.log(4 - 1);
+
+// 곱셈
+console.log(4 * 1);
+
+// 나눗셈
+console.log(4 / 1);
+
+// 나머지
+console.log(5 % 2);
+
+// 거듭제곱
+console.log(5 ** 2);
+
+// 우선순위 규칙을 따른다
+console.log(2 + 3 * 2);
+console.log(2 + (3 * 2));
