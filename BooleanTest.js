e00c5f25bb7d04759f0488472d0a9e970a409ab2
IDO <susususu07@naver.com>
2024-05-01 15:43:45 +0900

    Add files via upload

---
+++ BooleanTest.js
@@ -0,0 +1,29 @@
+// 불린 (Boolean)
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+
+// 일치하는가?
+console.log(2 === 2);
+// 불일치하는가?
+console.log(2 !== 2);
+
+// 일치하는가?
+console.log('AB' === 'AB');
+// 불일치하는가?
+console.log('AB' !== 'AB');
+
+// AND 연산 (&&)
+console.log(true && true)
+console.log(false && true)
+console.log(true && false)
+console.log(false && false)
+// OR 연산 (||)
+console.log(true || true)
+console.log(false || true)
+console.log(true || false)
+console.log(false || false)
+// NOT 연산 (!)
+console.log(!true)
+console.log(!false)
