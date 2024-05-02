8dfb843cf8bb047755a63c1e8a9ca1da7ae0883e
IDO <susususu07@naver.com>
2024-05-02 18:43:23 +0900

    Add files via upload

---
+++ typeConversion1.js
@@ -0,0 +1,9 @@
+// 형 변환(Type Conversion)
+// 함수 사용 String, Number, Boolean
+
+console.log('10' + '5') // String
+console.log(Number('10') + Number('5')) // Number
+
+console.log(10 + 5) // Number
+console.log(String(10) + String(5)) // String
+
