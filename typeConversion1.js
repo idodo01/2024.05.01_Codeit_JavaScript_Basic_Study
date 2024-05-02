06fce4d13ad293d55e09b1d196357525de29252b
IDO <susususu07@naver.com>
2024-05-02 18:44:46 +0900

    Update typeConversion1.js

--- typeConversion1.js
+++ typeConversion1.js
@@ -1,9 +1,9 @@
 // 형 변환(Type Conversion)
 // 함수 사용 String, Number, Boolean
 
-console.log('10' + '5') // String
-console.log(Number('10') + Number('5')) // Number
+console.log('10' + '5') // string
+console.log(Number('10') + Number('5')) // number
 
-console.log(10 + 5) // Number
-console.log(String(10) + String(5)) // String
+console.log(10 + 5) // number
+console.log(String(10) + String(5)) // string
 
