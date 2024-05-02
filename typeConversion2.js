0f3174463501cd1e46ded5701fbb84166ef9e87b
IDO <susususu07@naver.com>
2024-05-02 18:46:50 +0900

    Add files via upload

---
+++ typeConversion2.js
@@ -0,0 +1,21 @@
+// 형 변환(Type Conversion)
+
+// 숫자
+let x = 123;
+console.log(x);
+console.log(typeof x) // number
+
+// 숫자 -> 문자
+console.log(String(x));
+console.log(typeof String(x)) // string
+
+// 불린
+let y = true;
+console.log(y);
+console.log(typeof y) // boolean
+
+// 불린 -> 문자
+console.log(String(y));
+console.log(typeof String(y)) // string
+
+
