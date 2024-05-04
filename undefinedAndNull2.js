8e4b9c12145a8c48795b54fc1f39d9b97d2a3aaa
IDO <susususu07@naver.com>
2024-05-05 01:02:55 +0900

    Add files via upload

---
+++ undefinedAndNull2.js
@@ -0,0 +1,10 @@
+// undefined 과 null
+// undefined : 처음부터 값이 없었음
+// null : 의도적으로 값을 없도록 함
+
+let cup; // 컵에 아무것도 넣지 않음 
+console.log(cup); // undefined
+cup = '물'; // 컵에 물을 넣음
+console.log(cup); // '물'
+cup = null; // 컵을 비움
+console.log(cup); // null
