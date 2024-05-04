aa4e220388b240787931975ddb10ffb1207ea4b4
IDO <susususu07@naver.com>
2024-05-05 01:22:34 +0900

    Add files via upload

---
+++ orderOfFunctions1.js
@@ -0,0 +1,8 @@
+function sayHello() {
+  console.log('Hello');
+  console.log('Hi');
+}
+
+console.log('함수 호출 전')
+sayHello();
+console.log('함수 호출 후')
---
+++ orderOfFunctions2.js
@@ -0,0 +1,7 @@
+function square(x) {
+  return x * x;
+}
+
+console.log('함수 호출 전')
+console.log(square(5));
+console.log('함수 호출 후')
