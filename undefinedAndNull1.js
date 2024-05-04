87525c5dbdd6dfdb01e1cd0aeee4a5885c9c9e4a
IDO <susususu07@naver.com>
2024-05-05 01:01:14 +0900

    Add files via upload

---
+++ undefinedAndNull1.js
@@ -0,0 +1,14 @@
+// undefined 과 null
+// undefined : 처음부터 값이 없었음
+// null : 의도적으로 값을 없도록 함
+
+let codeit;
+console.log(codeit) // undefined
+
+codeit = null;
+console.log(codeit) // null
+
+// 일치 비교
+console.log(null == undefined) // true
+console.log(null === undefined) // false
+
