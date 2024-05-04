da50ffa22711f066b2e4d34a3df396b7cacfc7d6
IDO <susususu07@naver.com>
2024-05-05 02:40:15 +0900

    Add files via upload

---
+++ undefined_noParameters.js
@@ -0,0 +1,17 @@
+function sayHello(name) {
+  console.log(`안녕하세요 ${name}님`);
+}
+
+sayHello('코드잇')
+sayHello('아이두')
+sayHello() // undefined
+
+
+
+/*  <실행 결과>
+
+안녕하세요 코드잇님
+안녕하세요 아이두님
+안녕하세요 undefined님
+
+*/
