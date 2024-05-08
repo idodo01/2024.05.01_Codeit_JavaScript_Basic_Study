433855f295553762964102ea36e92ac778e5d094
IDO <susususu07@naver.com>
2024-05-08 21:14:25 +0900

    Add files via upload

---
+++ whileTest.js
@@ -0,0 +1,22 @@
+// while문 (while statment)
+let i = 1;
+
+while (i <= 10) {
+  console.log(`${i} 코드잇  최고~`);
+  i++;
+}
+
+// for문
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i} 코드잇  최고~`);
+}
+
+
+// for문보다 while문을 사용하는 것이 적합할 때 
+let j = 30;
+
+while (j % 7 != 0) {
+  j++;
+}
+
+console.log(j);
